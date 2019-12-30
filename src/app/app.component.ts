0e7ac463d97b8bb66b58f4ff90ec03d5e4f5364f
Avi Wasse <aviws198888@gmail.com>
2019-12-30 17:48:02 +0200

    CHANGES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular8-cours-project';
+  loadedFeature = "recipe";
+
+  onNavigate( feature: string ){
+    this.loadedFeature = feature;
+  }
 }
